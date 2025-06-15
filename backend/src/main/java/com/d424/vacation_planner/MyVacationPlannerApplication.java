0b4924c445e4eb2d36804e3c1eb1d5e1fb4c9dcb
mmarti58 <mmarti58@wgu.edu>
2025-06-15 16:27:28 -0700

    Initial project setup (Angular + Spring Boot)

---
+++ backend/src/main/java/com/d424/vacation_planner/MyVacationPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.d424.vacation_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVacationPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVacationPlannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/d424/vacation_planner/MyVacationPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d424.vacation_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVacationPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
