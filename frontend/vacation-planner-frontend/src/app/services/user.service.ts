862d5feb08ed723187ebd84fed6ac0699377cda5
mmarti58 <mmarti58@wgu.edu>
2025-06-19 10:32:46 -0700

    dev: create login service, add cors config to backend

---
+++ frontend/vacation-planner-frontend/src/app/services/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LoginRequest } from '../models/loginRequest.model';
+import { UserResponse } from '../models/userResponse.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private loginUrl = 'http://localhost:8080/api/users/login'
+  private registerUrl = 'http://localhost:8080/api/users'
+
+  constructor(private http: HttpClient) { }
+
+  loginUser(loginData: LoginRequest): Observable<UserResponse> {
+    return this.http.post<UserResponse>(this.loginUrl, loginData);
+  }
+}
--- frontend/vacation-planner-frontend/src/app/views/login-register/login-register.component.ts
+++ frontend/vacation-planner-frontend/src/app/views/login-register/login-register.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { LoginRequest } from '../../models/loginRequest.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-register',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login-register.component.html',
   styleUrl: './login-register.component.css'
 })
 export class LoginRegisterComponent {
+  email = '';
+  password = '';
+  loginError = '';
 
+  constructor(private userService: UserService, private router: Router) {}
+
+  onLogin(): void {
+    const loginData: LoginRequest = {
+      email: this.email,
+      password: this.password
+    };
+
+    // debug
+    console.log(loginData);
+
+    this.userService.loginUser(loginData).subscribe({
+      next: (user) => {
+        console.log('Logged in: ', user);
+        this.router.navigate(['/vacations']);
+      },
+      error: (err) => {
+        this.loginError = 'Login failed. Please check your credentials.';
+        console.error(err);
+      }
+    });
+
+  }
 }
