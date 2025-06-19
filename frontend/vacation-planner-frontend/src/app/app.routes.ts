3d13ca2151d45e7686589d0b52f12f283a0fac94
mmarti58 <mmarti58@wgu.edu>
2025-06-19 09:41:02 -0700

    fix: move login into component, update vacation-list

--- frontend/vacation-planner-frontend/src/app/app.routes.ts
+++ frontend/vacation-planner-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 // views
+import { LoginRegisterComponent } from './views/login-register/login-register.component';
 import { VacationListComponent } from './views/vacation-list/vacation-list.component';
 
 export const routes: Routes = [
-    { path: '/vacations', component: VacationListComponent },
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginRegisterComponent },
+    { path: 'vacations', component: VacationListComponent }
 ];
--- frontend/vacation-planner-frontend/src/app/views/vacation-list/vacation-list.component.ts
+++ frontend/vacation-planner-frontend/src/app/views/vacation-list/vacation-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { VacationService } from '../../services/vacation.service';
 import { Vacation } from '../../models/vacation.model';
 
 @Component({
   selector: 'app-vacation-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './vacation-list.component.html',
   styleUrl: './vacation-list.component.css'
 })
@@ -16,6 +17,7 @@ export class VacationListComponent implements OnInit {
   constructor(private vacationService: VacationService) {}
 
   ngOnInit(): void {
+      console.log("hello from vacation-lists")
       this.vacationService.getVacationbyUserId(this.userId).subscribe({
         next: (data) => (this.vacations = data),
         error: (err) => console.error('Error loading vacations:', err)
