0bfba3032012ce59535c736a934ae7935251c6ed
mmarti58 <mmarti58@wgu.edu>
2025-06-21 12:23:49 -0700

    dev: add search functionality

--- frontend/vacation-planner-frontend/src/app/views/vacation-list/vacation-list.component.ts
+++ frontend/vacation-planner-frontend/src/app/views/vacation-list/vacation-list.component.ts
@@ -5,10 +5,11 @@ import { Vacation } from '../../models/vacation.model';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vacation-list',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './vacation-list.component.html',
   styleUrl: './vacation-list.component.css'
 })
@@ -16,12 +17,26 @@ export class VacationListComponent implements OnInit {
 
   vacations: Vacation[] = [];
   user: any = {};
+  searchTerm: string = '';
 
   constructor(
     private vacationService: VacationService, 
     private authService: AuthService,
     private router: Router
   ) {}
+  // https://www.geeksforgeeks.org/how-to-use-getters-setters-in-typescript/
+  get filteredVacations(): Vacation[] {
+    if (!this.searchTerm.trim()) {
+      return this.vacations;
+    }
+
+    const term = this.searchTerm.toLowerCase();
+
+    return this.vacations.filter(vacation =>
+      vacation.title.toLowerCase().includes(term)
+    );
+  }
+
 
   ngOnInit(): void {
     this.loadVacations();
