18a686cb95e197700346a994434f27359319588a
marcosetm <m2e5090@gmail.com>
2025-06-22 17:21:21 -0700

    fix: update urls

--- frontend/vacation-planner-frontend/src/app/services/user.service.ts
+++ frontend/vacation-planner-frontend/src/app/services/user.service.ts
@@ -4,13 +4,14 @@ import { Observable } from 'rxjs';
 import { LoginRequest } from '../models/loginRequest.model';
 import { UserResponse } from '../models/userResponse.model';
 import { RegisterRequest } from '../models/registerRequest.model';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private loginUrl = 'http://localhost:8080/api/users/login'
-  private registerUrl = 'http://localhost:8080/api/users'
+  private loginUrl = `${environment.apiUrl}/users/login`;
+  private registerUrl = `${environment.apiUrl}/users`;
 
   constructor(private http: HttpClient) { }
 
