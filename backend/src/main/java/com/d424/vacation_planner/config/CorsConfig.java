9a3a424b1e61a2f550963a7cbd1afde8427739a7
marcosetm <m2e5090@gmail.com>
2025-06-22 17:42:57 -0700

    fix: re-build backend

--- backend/src/main/java/com/d424/vacation_planner/config/CorsConfig.java
+++ backend/src/main/java/com/d424/vacation_planner/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns(allowedOrigins)
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
