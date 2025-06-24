2054596eb4bccaef1e70d5e7150e74d54cce9e51
marcosetm <m2e5090@gmail.com>
2025-06-23 20:29:24 -0700

    D. Software Testing

--- backend/src/test/java/com/d424/vacation_planner/LoginTest.java
+++ backend/src/test/java/com/d424/vacation_planner/LoginTest.java
@@ -15,8 +15,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class LoginTest {
 
     @Autowired
-    private UserService userService;
-    @Autowired
     private UserController userController;
 
     @Test
